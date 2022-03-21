b8bef0041721300171cbfc3dd89aa81f68d0c2e0
sandayjulie <85165965+sandayjulie@users.noreply.github.com>
2022-03-21 13:04:54 -0300

    Add files via upload

---
+++ routes/mygrid.js
@@ -0,0 +1,26 @@
+module.exports = (app) => {
+    
+    //importar a configuração do database
+    var conexao = require ('../config/database')
+
+    //executar a conexao 
+    conexao()
+
+    //importar o modelo mygrid
+    var mygrid = require ('../models/mygrid')
+
+    //abrir o formulário
+    app.get('/mygrid',(req,res) => {
+        res.render('mygrid.ejs')
+    })
+
+    //gravar as informações do formulário no banco de dados
+    app.post('/mygrid', (req,res) => {
+        var documento = new mygrid ({
+            titulo:req.body.titulo,
+            texto:req.body.texto
+        }).save()
+        .then(()=>{ res.redirect('/mygrid')})
+        .catch(()=>{res.send('Não foi possível gravar')})
+    })
+}
